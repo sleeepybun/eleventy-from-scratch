468a898738e5684dfb61eb422eb3bb7924caef4a
Hanan Shoubaki <hananshoubaki@gmail.com>
2022-11-10 10:25:33 -0600

    added image optimization task to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const {parallel, watch} = require('gulp');
 
 const fonts = require('./gulp-tasks/fonts.js');
 
+const images = require('./gulp-tasks/images.js');
 // Pull in each task
 const sass = require('./gulp-tasks/sass.js');
 
@@ -10,11 +11,12 @@ const sass = require('./gulp-tasks/sass.js');
 // prevent the task being run when we run `gulp watch`, but it
 // will run when a file changes.
 const watcher = () => {
+  watch('./src/images/**/*', {ignoreInitial: true}, images);
   watch('./src/scss/**/*.scss', {ignoreInitial: true}, sass);
 };
 
 // The default (if someone just runs `gulp`) is to run each task in parrallel
-exports.default = parallel(fonts, sass);
+exports.default = parallel(fonts, images, sass);
 
 // This is our watcher task that instructs gulp to watch directories and
 // act accordingly
