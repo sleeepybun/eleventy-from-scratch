57862a181bbf8ef0bbb38bb4ab910c883ca2f5f8
Hanan Shoubaki <hananshoubaki@gmail.com>
2022-11-17 14:37:51 -0600

    installed html-minifier and added to eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,11 @@ const rssPlugin = require('@11ty/eleventy-plugin-rss');
 // Filters
 const dateFilter = require('./src/filters/date-filter.js');
 const w3DateFilter = require('./src/filters/w3-date-filter.js');
+// Transforms 
+const htmlMinTransform = require('./src/transforms/html-min-transform.js');
+
+// Create a helpful production flag
+const isProduction = process.env.NODE_ENV === 'production';
 
 const sortByDisplayOrder = require('./src/utils/sort-by-display-order.js');
 
